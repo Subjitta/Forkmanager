20334f7a6fd510f55fb098b8e1296da5495411d0
Вадим <subjitta0@gmail.com>
2025-07-10 12:25:23 +0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,25 @@
 import os
 
-from utils.forkutil import ForkRoot
-
 import click
+
+from utils.forkutil import ForkRoot
+from utils.textutil import *
 
 @click.group()  # Основная группа с командами
 def clipr():
     pass
 
 @clipr.command()
-@click.argument('project name')
+@click.argument('project_name')
 def create(project_name: str) -> None:
-    ForkRoot.open_root() # Открываем fork-root
-    ForkRoot.add_workspace(name=project_name)
+    os.path.join(ForkRoot.get_root()) # Открываем fork-root
+
+    workspace_name = make_str(project_name)
+
+    ForkRoot.add_workspace(
+        name=project_name,
+        workspace=workspace_name # Имя workspace
+    )
+
+if __name__ == "__main__":
+    clipr()
