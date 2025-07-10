b5dee182dc4cdce9356d2152a8ca6e3a62772d99
Вадим <subjitta0@gmail.com>
2025-07-10 11:15:01 +0300

    Changed work with workspaces

--- src/utils/forkutil.py
+++ src/utils/forkutil.py
@@ -2,11 +2,25 @@ import os
 
 class ForkRoot(): # Работаем с forkmanager рут
     @staticmethod
-    def open_root() -> None:
+    def get_root() -> str:
         WINDOWS_PATH = "%USERPROFILE%/.forkmanager" # Стандартный путь для винды
+        return WINDOWS_PATH
 
-        os.path.join(WINDOWS_PATH)
+    @staticmethod
+    def open_workspaces() -> None:
+        os.path.join(ForkRoot.get_root() + "workspaces") # Присоеденяем
 
     @staticmethod
     def add_workspace(**attrs) -> bool:
-        return True     # Пока еще не сдеолано продолжу завтра
+        """
+        Схема создания рабочего пространства:
+
+        - Создание внешней директории
+        - Заполнение конфига пространства
+        - Создание проекта.
+        - Включение пространства в json конфиг
+
+        функция возвращает тру если все удалось
+        функция возвращает фолс если были ошибки  
+        """
+        pass
